a06c3fd8e60fc7c7c96103f1d632412d022de58e
cesaraugusto87 <cesara87@gmail.com>
2016-10-02 21:09:58 -0400

    Android First Test

--- app/src/main/java/cesaraugusto87/androidtestapp/PrincipalPage.java
+++ app/src/main/java/cesaraugusto87/androidtestapp/PrincipalPage.java
@@ -3,18 +3,20 @@ package cesaraugusto87.androidtestapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.view.View;
 
 public class PrincipalPage extends AppCompatActivity {
-    private EditText email;
+    private EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal_page);
         email=(EditText)findViewById(R.id.username);
-        
+        password=(EditText)findViewById(R.id.password);
     }
 
-    private void showNextPage(){
+    public void showNextPage(View view){
+
     }
 }
